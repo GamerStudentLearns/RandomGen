824d3019c62d20ff3ee3a8cbfd106495dc755bed
GamerStudentLearns <767475@newcollege.ac.uk>
2026-03-11 19:33:46 +0000

    fixed small bugs with item descriptions

--- Assets/Scripts/Items/AbsorbentSponge.cs
+++ Assets/Scripts/Items/AbsorbentSponge.cs
@@ -4,44 +4,25 @@ using System.Collections;
 [CreateAssetMenu(menuName = "Items/Absorbent Sponge")]
 public class AbsorbentSponge : ItemData
 {
-    private bool subscribed;
-    private int absorbed;
+    
 
     public override void OnPickup(PlayerStats stats, RunManager run)
     {
-        Subscribe(stats);
+        stats.ModifyStat(s =>
+        {
+            s.shotSpeed += 1.5f;
+            s.fireRate -= 0.1f;
+        });
     }
 
     public override void ApplyPersistent(PlayerStats stats, RunManager run)
     {
-        Subscribe(stats);
-    }
-
-    private void Subscribe(PlayerStats stats)
-    {
-        if (subscribed) return;
-        subscribed = true;
-
-        ProjectileEvents.OnEnemyProjectileDestroyed += () =>
+        stats.ModifyStat(s =>
         {
-            absorbed++;
-            if (absorbed >= 5)
-            {
-                absorbed = 0;
-                stats.StartCoroutine(SafeStatChange(stats, s =>
-                {
-                    s.damage += 1f;
-                }));
-            }
-        };
-
-        RoomEvents.OnRoomEntered += (_) => absorbed = 0;
+            s.shotSpeed += 1.5f;
+            s.fireRate -= 0.1f;
+        });
     }
 
-    private IEnumerator SafeStatChange(PlayerStats stats, System.Action<PlayerStats> change)
-    {
-        yield return null;
-        yield return null;
-        stats.ModifyStat(change);
-    }
+    
 }
